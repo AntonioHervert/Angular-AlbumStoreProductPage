7b0e90b7659f0d093163d8d96d487b2c7bfe7e8c
AntonioHervert <mcantuan_guns@live.com.mx>
2020-10-19 18:44:53 -0500

    Data from json

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  
+  private albumInfo;
+  constructor(private _productServie: ProductService) { }
 
   ngOnInit() {
+    this._productServie.getAlbum(1).subscribe(response=> {
+      console.log(response)
+      this.albumInfo=response});
   }
 
 }
